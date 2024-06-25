89f9a529ffb5cf5122e20804ef87d61bbcf57c86
Amlan Sahoo <35356517+amlansahoo07@users.noreply.github.com>
2024-06-25 23:12:02 +0530

    Add html, css, js and asset files

---
+++ index.js
@@ -0,0 +1,20 @@
+var randomNumber1 = Math.floor(((Math).random() * 6) + 1);
+img1Name = "./images/dice" + randomNumber1 + ".png";
+document.getElementsByClassName("img1")[0].setAttribute("src", img1Name);
+
+var randomNumber2 = Math.floor(((Math).random() * 6) + 1);
+img2Name = "./images/dice" + randomNumber2 + ".png";
+document.getElementsByClassName("img2")[0].setAttribute("src", img2Name);
+
+var result = "";
+if (randomNumber1 === randomNumber2) {
+    result = "Draw!";
+}
+else if (randomNumber1 < randomNumber2) {
+    result = "Player 2 Wins! 🏆";
+}
+else {
+    result = "🏆 Player 1 Wins!";
+}
+
+document.querySelector("h1").innerHTML = result;
